6e54477a3301666e6ce53c727d898acb88c6eba8
guniism <gun16110@hotmail.com>
2025-03-17 22:49:21 +0700

    Finish assignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-import styles from "./page.module.css";
 import Banner from "@/components/Banner";
-import CardPanel from "@/components/CardPanel";
 
 export default function Home() {
   return (
-    <main className={styles.main} >
+    <main className="flex flex-col justify-between">
       
       <Banner />
-        <div className="max-w-6xl w-full mx-auto px-4">
-          <CardPanel />
-        </div>
-
+      
     </main>
   );
 }
--- src/components/CardPanel.tsx
+++ src/components/CardPanel.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useReducer } from "react";
 import Card from "./Card";
+import Link from "next/link";
 
 export default function CardPanel(){
     const listReducer = (list:Map<string, number>, action:{type:string, venueName:string, value:number}) => {
@@ -26,13 +27,31 @@ export default function CardPanel(){
 
       
     const [ mapList, dispatchCompare ] = useReducer(listReducer, initialMap);
+    
+    /*
+     * Mock Data for Demontration only 
+     */
+    const mockVenueRepo = [
+        {vid: "001", name: "The Bloom Pavilion", image: "/img/bloom.jpg"},
+        {vid: "002", name: "Spark Space", image: "/img/sparkspace.jpg"},
+        {vid: "003", name: "The Grand Table", image: "/img/grandtable.jpg"}
+    ]
+
     return(
         <div>
             <div style={{margin:"20px", display:"flex", flexDirection:"row", flexWrap:"wrap", justifyContent:"space-around", alignContent:"space-around", gap: "20px"}}>
-                <Card venueName="The Bloom Pavilion" imgSrc="/img/bloom.jpg" onUpdateList={(name: string, value: number) => dispatchCompare({ type: "add", venueName: name, value: value })}/>
-                <Card venueName="Spark Space" imgSrc="/img/sparkspace.jpg" onUpdateList={(name: string, value: number) =>dispatchCompare({ type: "add", venueName: name, value: value })}/>
-                <Card venueName="The Grand Table" imgSrc="/img/grandtable.jpg" onUpdateList={(name: string, value: number) =>dispatchCompare({ type: "add", venueName: name, value: value })}/>
-                {/* <Card venueName="The Grand Table" imgSrc="/img/grandtable.jpg" /> */}
+                {/* <Card venueName="The Bloom Pavilion" imgSrc="/img/bloom.jpg" onUpdateList={(name: string, value: number) => dispatchCompare({ type: "add", venueName: name, value: value })}/>
+                <Card venueName="Spark Space" imgSrc="/img/sparkspace.jpg" onUpdateList={(name: string, value: number) => dispatchCompare({ type: "add", venueName: name, value: value })}/>
+                <Card venueName="The Grand Table" imgSrc="/img/grandtable.jpg" onUpdateList={(name: string, value: number) => dispatchCompare({ type: "add", venueName: name, value: value })}/> */}
+                {
+                    mockVenueRepo.map((venueItem) => (
+                        <Link key={venueItem.vid}  href={`/venue/${venueItem.vid}`}>
+                            <Card venueName={venueItem.name} imgSrc={venueItem.image} 
+                                onUpdateList={(name: string, value: number) => dispatchCompare({ type: "add", venueName: name, value: value })}
+                            />
+                        </Link>
+                    ))
+                }
             </div>
             <div>
                 <b>Venue List with Ratings : {3}</b>
